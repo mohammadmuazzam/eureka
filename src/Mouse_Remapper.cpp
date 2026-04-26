e3aa5d3faec1d05bcf392550be208109a8b68408
Mohammad Muazzam Ali <mohammadmuazzamalii@gmail.com>
2026-04-26 17:36:48 +0800

    copied bits from physical device to virtual device, added a remap function

--- src/Mouse_Remapper.cpp
+++ src/Mouse_Remapper.cpp
@@ -1,15 +1,28 @@
 //* one remapper per device
 #include <map>
 #include <string>
+#include <linux/input.h>
 
 using namespace std;
 
 class Mouse_Remapper {
     public:
         Mouse_Remapper();
+
+        int get_remap_code(struct input_event ev)
+        {
+            if (code_mapping.find(ev.code) != code_mapping.end())
+            {
+                return code_mapping[ev.code];
+            }
+            return -1;
+        }
+
     private:
-        //* from original device to virtual device
+        //* original device -> virtual device
         string device_name;
-        map <unsigned short, unsigned short> mapping;
-        int threshold_x, threshold_y; //* for scroll sensitivity
+        map <unsigned short, unsigned short> code_mapping;
+        int cthreshold_x, cthreshold_y,                         //* for cursor sensitivity
+            sthreshold_x, sthreshold_y;                         //* for scroll sensitivity
+
 };
--- src/Virtual_Device.cpp
+++ src/Virtual_Device.cpp
@@ -1,3 +1,78 @@
-class Virtual_Device {
+#include <linux/uinput.h>
+#include <string>
+#include <fcntl.h>
+#include <iostream>
+#include <cstring>
 
+using namespace std;
+
+#define BITS_PER_LONG (sizeof(long) * 8)
+#define NLONGS(x) ((x + BITS_PER_LONG - 1) / BITS_PER_LONG)
+
+class Virtual_Device {
+    private:
+        std::string device_name;
+        int fd;
+
+        struct uinput_setup setup_mouse(string name, int physical_mouse_fd)
+        {
+            struct uinput_setup usetup;
+            memset(&usetup, 0, sizeof(usetup));
+            unsigned long key_bits[NLONGS(KEY_CNT)] = {0};
+            unsigned long rel_bits[NLONGS(REL_CNT)] = {0};
+
+            if (ioctl(physical_mouse_fd, EVIOCGBIT(EV_REL, sizeof(rel_bits)), rel_bits) < 0 ||
+                ioctl(physical_mouse_fd, EVIOCGBIT(EV_KEY, sizeof(key_bits)), key_bits) < 0)
+            {
+                cerr << "Error getting mouse bits" << endl;
+                return {0};
+            }
+
+            ioctl(fd, UI_SET_EVBIT, EV_SYN);
+            ioctl(fd, UI_SET_RELBIT, EV_REL);
+            ioctl(fd, UI_SET_EVBIT, EV_KEY);
+
+            //* set relative bits
+            for (int i = 0; i < REL_CNT; i++) 
+            {
+                if (rel_bits[i / BITS_PER_LONG] & 1UL << (i % BITS_PER_LONG))
+                {
+                    ioctl(fd, UI_SET_RELBIT, i);
+                }
+            }
+
+            //* set key bits
+            for (int i = 0; i < KEY_CNT; i++)
+            {
+                if (key_bits[i / BITS_PER_LONG] & 1UL << (i % BITS_PER_LONG))
+                {
+                    ioctl(fd, UI_SET_KEYBIT, i);
+                }
+            }            
+            
+            usetup.id.bustype = BUS_USB;
+            usetup.id.vendor = 0xDEAD;
+            usetup.id.product = 0xBEEF;
+            strcpy(usetup.name, name.c_str());
+
+            ioctl(fd, UI_DEV_SETUP, &usetup);
+            ioctl(fd, UI_DEV_CREATE);
+
+            return usetup;
+        }
+
+
+    public:
+        struct uinput_setup usetup;
+        Virtual_Device(string name, int physical_mouse_fd) : device_name(name)
+        {
+            fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+            if (fd < 0) 
+            {
+                cerr << "Failed to open uinput device" << endl;
+                return;
+            }
+
+            setup_mouse(name, physical_mouse_fd);
+        }
 };
