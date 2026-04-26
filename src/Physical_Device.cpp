d83fc56ec2fc328b18c51de4b8c6cd32d7ae6442
Mohammad Muazzam Ali <mohammadmuazzamalii@gmail.com>
2026-04-26 15:50:17 +0800

    added read_event in physical_dev

--- src/Physical_Device.cpp
+++ src/Physical_Device.cpp
@@ -1,5 +1,9 @@
 #include <string>
 #include <vector>
+#include <linux/input.h>
+#include <unistd.h>
+#include <iostream>
+
 
 using namespace std;
 
@@ -7,7 +11,19 @@ class Physical_Device
 {
     private:
         string device_name;
+        int fd;
                 
     public:
         static vector<Physical_Device> get_connected_devices();
+        
+        struct input_event read_event()
+        {
+            struct input_event ev;
+            ssize_t read_bytes = read(fd, &ev, sizeof(struct input_event));
+            if (read_bytes < 0) {
+                cerr << "Failed to read from device " << device_name << endl;
+                return {};
+            }
+            return ev;
+        };
 };
